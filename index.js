e860e5647045d2ed80f18455ba9564aa8c15e56e
Dean Howe <hideanhowe@gmail.com>
2025-11-27 16:44:10 +0000

    logic

--- index.js
+++ index.js
@@ -4,3 +4,17 @@
 // Assume all birthdays are valid dates before November 27th, 2025.
 // Return the age as an integer.
 // Be sure to account for whether the person has already had their birthday in 2025.
+
+function calculateAge(birthday) {
+  console.log(birthday);
+  const birthdayDate = new Date(birthday);
+  console.log(2025 - birthdayDate.getFullYear());
+  return birthday;
+}
+
+// getFullYear() returns the full year of a date.
+
+// Get the year of a specific date:
+
+// const d = new Date("July 21, 1983 01:15:00");
+// let year = d.getFullYear();
